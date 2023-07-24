3caaf0dff07e268779e055141a327e287bb43339
cassiegraf <cassiegraf@gmail.com>
2023-07-23 19:26:03 -0700

    back to original code, when I tried adding circle markers the map disappeared

--- Leaflet-Part-1/part-1.js
+++ Leaflet-Part-1/part-1.js
@@ -5,91 +5,63 @@ d3.json(url).then(function (data) {
     console.log(data.features);
 
     createFeatures(data.features);
-})
+});
 
 function createFeatures(earthquakeData) {
     console.log("Earthquake Data:")
     console.log(earthquakeData);
-}
 
-function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><h4>Magnitude: ${feature.properties.mag} | Depth: ${feature.geometry.coordinates[2]}<h4>`);
-    
-    let earthquakes = L.geoJSON(earthquakeData, {
-        style: circle, 
+    // bind the location and magnitude from the data
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><h4>Magnitude: ${feature.properties.mag} | Depth:${feature.geometry.coordinates[2]}<h4>`);
+      }
+
+      let earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature
-        })
+      });
 
-    // create circle marker, bind the location, magnitude, and depth from the data
-    let circle = L.circleMarker([feature.geometry.coordinates[0], feature.geometry.coordinates[1]], {
-        radius: feature.geometry.coordinates[2],
-        color: getColor,
-        opacity: 0.5
-    })
+      createMap(earthquakes);
+    }
 
-        createMap(earthquakes);
+    function createMap(earthquakes) {
 
-}
+        let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          })
 
-    function getColor(depth) {
-        switch (true) {
-          case depth > 90:
-            return "#EA2C2C";
-          case depth > 70:
-            return "#EA822C";
-          case depth > 50:
-            return "#EE9C00";
-          case depth > 30:
-            return "#EECC00";
-          case depth > 10:
-            return "#D4EE00";
-          default:
-            return "#98EE00";
-        }
-      } 
-    
+          let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+          });
 
 
 
 
-    
-function createMap(earthquakes) {
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
 
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        })
-          
-    // create map, centered on Denver, add layers
-    let myMap = L.map("map", {
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // centered map at Denver, CO coordinates
+  let myMap = L.map("map", {
     center: [
         39.7643389,-104.8551114
     ],
     zoom: 4.5,
-    layers: [
-        street,    
-        earthquakes,
-        circle
-    ]
-    })}
+    layers: [street, earthquakes]
+  });
 
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 
-    // create the legend
-    let legend = L.control({
-    position: "bottomright"
-    })
-    
-        
-    
-
-   
-
-
-
-  
-
-
-
-
-
-
+}
 
