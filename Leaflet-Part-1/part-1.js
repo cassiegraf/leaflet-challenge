4e6ed62d5b7dd75ba65e2c81cda37fba1fbd91cf
cassiegraf <cassiegraf@gmail.com>
2023-07-23 18:16:27 -0700

    added code for markers but map disappeared

--- Leaflet-Part-1/part-1.js
+++ Leaflet-Part-1/part-1.js
@@ -10,25 +10,55 @@ d3.json(url).then(function (data) {
 function createFeatures(earthquakeData) {
     console.log("Earthquake Data:")
     console.log(earthquakeData);
+}
 
-    // bind the location, magnitude, and depth from the data
-    function onEachFeature(feature, layer) {
+function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><h4>Magnitude: ${feature.properties.mag} | Depth: ${feature.geometry.coordinates[2]}<h4>`);
-    }  
+    
     let earthquakes = L.geoJSON(earthquakeData, {
-        style: mapStyle, 
+        style: circle, 
         onEachFeature: onEachFeature
         })
 
+    // create circle marker, bind the location, magnitude, and depth from the data
+    let circle = L.circleMarker([feature.geometry.coordinates[0], feature.geometry.coordinates[1]], {
+        radius: feature.geometry.coordinates[2],
+        color: getColor,
+        opacity: 0.5
+    })
+
         createMap(earthquakes);
+
 }
+
+    function getColor(depth) {
+        switch (true) {
+          case depth > 90:
+            return "#EA2C2C";
+          case depth > 70:
+            return "#EA822C";
+          case depth > 50:
+            return "#EE9C00";
+          case depth > 30:
+            return "#EECC00";
+          case depth > 10:
+            return "#D4EE00";
+          default:
+            return "#98EE00";
+        }
+      } 
+    
+
+
+
+
     
 function createMap(earthquakes) {
 
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         })
-        
+          
     // create map, centered on Denver, add layers
     let myMap = L.map("map", {
     center: [
@@ -37,39 +67,21 @@ function createMap(earthquakes) {
     zoom: 4.5,
     layers: [
         street,    
-        earthquakes
+        earthquakes,
+        circle
     ]
-    })
+    })}
+
+
     // create the legend
     let legend = L.control({
     position: "bottomright"
     })
     
         
-    } 
+    
 
-function mapStyle(feature) {
-    return{
-    fillColor: getColor(feature.geometry.coordinates[2])
-    };
-}      
-
-function getColor(depth) {
-    switch (true) {
-      case depth > 90:
-        return "#EA2C2C";
-      case depth > 70:
-        return "#EA822C";
-      case depth > 50:
-        return "#EE9C00";
-      case depth > 30:
-        return "#EECC00";
-      case depth > 10:
-        return "#D4EE00";
-      default:
-        return "#98EE00";
-    }
-  }    
+   
 
 
 
